609c8b1a95f3a4d5a674b126f38c4d90e1a28429
MERNdevAjay04 <developerajaymehra04@gmail.com>
2023-12-28 00:14:24 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-// import React from 'react'
-// import './App.css'
-// import test from './components/test'
-// function App(){
-//      const name = "Bean"
-//      return(
-//         <div className= 'App'>
-//         <h2>Hello Mr.{name}</h2>
-//         <p>Lorem ipsum dolor</p>
-//         </div>
-//      )
-// }
-// export default App
+import React from 'react'
+import './App.css'
+import test from './components/test'
+function App(){
+     const name = "Bean"
+     return(
+        <div className= 'App'>
+        <h2>Hello Mr.{name}</h2>
+        <p>Lorem ipsum dolor</p>
+          <button>Send</button>
+        </div>
+     )
+}
+export default App
 
